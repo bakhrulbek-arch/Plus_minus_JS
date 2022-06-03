39a6a020e1127895ec4e4b4ae6c2cb8e32bcee7f
Bakhrulbek <arch_ghost@hotmail.com>
2022-06-03 19:34:32 +0500

    plus_minus_js_config

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ let h1 = document.querySelector("h1")
 
 let min = document.querySelector(".min")
 min.onclick = () => {
-    h1.innerHTML = num--
-    console.log(num);
+    h1.innerHTML = --num
 }
 
 let max = document.querySelector(".max")
 max.onclick = () => {
-    h1.innerHTML = num ++
-    console.log(num);
+    h1.innerHTML = ++num
 }
 
 
@@ -19,21 +17,18 @@ let min_5 = document.querySelector(".min_5")
 min_5.onclick = () => {
     num = num - 5
     h1.innerHTML = num
-    console.log(num);
 }
 
 let min_10 = document.querySelector(".min_10")
 min_10.onclick = () => {
     num = num - 10
     h1.innerHTML = num
-    console.log(num);
 }
 
 let min_100 = document.querySelector("#clc_min")
 min_100.onclick = () => {
     num = num - 100
     h1.innerHTML = num
-    console.log(num);
 }
 
 
@@ -43,21 +38,18 @@ let max_5 = document.querySelector(".max_5")
 max_5.onclick = () => {
     num = num + 5
     h1.innerHTML = num
-    console.log(num);
 }
 
 let max_10 = document.querySelector(".max_10")
 max_10.onclick = () => {
     num = num + 10
     h1.innerHTML = num
-    console.log(num);
 }
 
 let max_100 = document.querySelector("#clc_max")
 max_100.onclick = () => {
     num = num + 100
     h1.innerHTML = num
-    console.log(num);
 }
 
 let clr = document.querySelector(".clr")
